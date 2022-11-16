f130f1f8b3e7e40f437736bc4f1d370bafb6ab7b
Bui Gia An <buigiaanfb1@gmail.com>
2022-11-16 09:58:02 +0700

    Prevent cheat

--- src/components/box/BoxList.js
+++ src/components/box/BoxList.js
@@ -5,51 +5,56 @@ import { addItem } from '../../features/cart/cartSlice';
 const BoxList = ({ boxs }) => {
   const dispatch = useDispatch();
 
+  console.log(boxs);
+
   return (
     <div className="box-list">
       {boxs &&
-        boxs.map((item) => (
-          <div className="pattern_coll" key={item.boxId}>
-            <div className="nft_wrap">
-              <span>
-                <img
-                  src={item.image}
-                  className="lazy img-fluid"
-                  alt=""
-                  style={{
-                    objectFit: 'cover',
-                    height: '170px',
-                    width: '100%',
-                  }}
-                />
-              </span>
-            </div>
-            <div className="nft_coll_info">
-              <br />
-              <span>
-                <Link to={`/boxdetail/${item.boxId}`}>
-                  <h4
-                    style={{
-                      whiteSpace: 'nowrap',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                    }}
+        boxs.map(
+          (item) =>
+            item.status && (
+              <div className="pattern_coll" key={item.boxId}>
+                <div className="nft_wrap">
+                  <span>
+                    <img
+                      src={item.image}
+                      className="lazy img-fluid"
+                      alt=""
+                      style={{
+                        objectFit: 'cover',
+                        height: '170px',
+                        width: '100%',
+                      }}
+                    />
+                  </span>
+                </div>
+                <div className="nft_coll_info">
+                  <br />
+                  <span>
+                    <Link to={`/boxdetail/${item.boxId}`}>
+                      <h4
+                        style={{
+                          whiteSpace: 'nowrap',
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                        }}
+                      >
+                        {item.name}
+                      </h4>
+                    </Link>
+                  </span>
+                  <span>{item.price}.000VNĐ</span>
+                  <span>&nbsp;</span>
+                  <span
+                    className="btn-main lead"
+                    onClick={() => dispatch(addItem(item))}
                   >
-                    {item.name}
-                  </h4>
-                </Link>
-              </span>
-              <span>{item.price}.000VNĐ</span>
-              <span>&nbsp;</span>
-              <span
-                className="btn-main lead"
-                onClick={() => dispatch(addItem(item))}
-              >
-                Add to Cart
-              </span>
-            </div>
-          </div>
-        ))}
+                    Add to Cart
+                  </span>
+                </div>
+              </div>
+            )
+        )}
     </div>
   );
 };
